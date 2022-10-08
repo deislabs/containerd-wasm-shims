b0335a1c009ae37835a41f9e3c73b46f7ec40eb0
Jiaxiao Zhou <jiazho@microsoft.com>
2022-10-08 06:48:05 -0700

    feat:: updated spin to the latest commit (#25)
    * updated spin to the latest commit
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

---
+++ containerd-shim-spin-v1/src/podio.rs
@@ -0,0 +1,59 @@
+use std::{path::PathBuf, fs::{OpenOptions, File}};
+
+use spin_trigger::TriggerHooks;
+
+pub struct PodioLoggingTriggerHooks {
+    stdout_pipe: Option<File>,
+    stderr_pipe: Option<File>,
+    stdin_pipe: Option<File>,
+}
+
+fn maybe_open_stdio(pipe_path: &PathBuf) -> Option<std::fs::File> {
+    if pipe_path.as_os_str().is_empty() {
+        None
+    } else {
+        Some(
+            OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open(pipe_path.clone())
+                .expect("could not open pipe"),
+        )
+    }
+}
+
+impl PodioLoggingTriggerHooks {
+    pub fn new(stdout_pipe_path: PathBuf, stderr_pipe_path: PathBuf, stdin_pipe_path: PathBuf) -> Self {
+        let stdout_pipe = maybe_open_stdio(&stdout_pipe_path);
+        let stderr_pipe = maybe_open_stdio(&stderr_pipe_path);
+        let stdin_pipe = maybe_open_stdio(&stdin_pipe_path);
+        Self {
+            stdout_pipe,
+            stderr_pipe,
+            stdin_pipe,
+        }
+    }
+}
+
+impl TriggerHooks for PodioLoggingTriggerHooks {
+    fn app_loaded(&mut self, _app: &spin_app::App) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn component_store_builder(
+        &self,
+        _component: spin_app::AppComponent,
+        builder: &mut spin_core::StoreBuilder,
+    ) -> anyhow::Result<()> {
+        if let Some(stdout_pipe) = &self.stdout_pipe {
+            builder.stdout_pipe(stdout_pipe.try_clone().unwrap());
+        }
+        if let Some(stderr_pipe) = &self.stderr_pipe {
+            builder.stderr_pipe(stderr_pipe.try_clone().unwrap());
+        }
+        if let Some(stdin_pipe) = &self.stdin_pipe {
+            builder.stdin_pipe(stdin_pipe.try_clone().unwrap());
+        }
+        Ok(())
+    }
+}
--- images/spin/src/lib.rs
+++ images/spin/src/lib.rs
@@ -6,6 +6,7 @@ use spin_sdk::{
 
 #[http_component]
 fn hello_world(_req: Request) -> Result<Response> {
+    println!("Hello, world! You should see me in pod logs");
     Ok(http::Response::builder()
         .status(200)
         .body(Some("Hello world from Spin!".into()))?)
