3bf93b0fd12097fde9d0e856abe7d88e114652ae
Jiaxiao Zhou <jiazho@microsoft.com>
2022-10-11 07:16:39 -0700

    added dotnet image (#30)
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

---
+++ images/spin_dotnet/Handler.cs
@@ -0,0 +1,20 @@
+﻿using Fermyon.Spin.Sdk;
+
+namespace MyTestProject;
+
+public static class Handler
+{
+    [HttpHandler]
+    public static HttpResponse HandleHttpRequest(HttpRequest request)
+    {
+        return new HttpResponse
+        {
+            StatusCode = System.Net.HttpStatusCode.OK,
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "text/plain" },
+            },
+            BodyAsString = "Hello from .NET\n",
+        };
+    }
+}
