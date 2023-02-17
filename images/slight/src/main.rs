e3c8539dcb8940dc1f403655032ffbb31011e5af
jiaxiao zhou <jiazho@microsoft.com>
2023-02-17 23:26:09 +0000

    updated slight and spin images
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- images/slight/src/main.rs
+++ images/slight/src/main.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
 
-use http::*;
-use kv::*;
+use http_server::*;
+use keyvalue::*;
 use slight_http_handler_macro::register_handler;
 
-wit_bindgen_rust::import!("http.wit");
-wit_bindgen_rust::import!("kv.wit");
-wit_error_rs::impl_error!(http::Error);
-wit_error_rs::impl_error!(kv::Error);
+wit_bindgen_rust::import!("http-server.wit");
+wit_bindgen_rust::import!("keyvalue.wit");
+wit_error_rs::impl_error!(http_server::HttpRouterError);
+wit_error_rs::impl_error!(keyvalue::KeyvalueError);
 
 fn main() -> Result<()> {
     let router = Router::new()?;
@@ -20,14 +20,13 @@ fn main() -> Result<()> {
 
     println!("guest starting server");
     let _ = Server::serve("0.0.0.0:80", &router_with_route)?;
-    // server.stop().unwrap();
     println!("guest moving on");
 
     Ok(())
 }
 
 #[register_handler]
-fn handle_hello(req: Request) -> Result<Response, Error> {
+fn handle_hello(req: Request) -> Result<Response, HttpError> {
     Ok(Response {
         headers: Some(req.headers),
         body: Some("hello".as_bytes().to_vec()),
@@ -36,32 +35,43 @@ fn handle_hello(req: Request) -> Result<Response, Error> {
 }
 
 #[register_handler]
-fn handle_foo(request: Request) -> Result<Response, Error> {
-    let kv = crate::Kv::open("my-container").unwrap();
-    let value = kv.get("key").unwrap();
-    Ok(Response {
-        headers: Some(request.headers),
-        body: Some(value),
-        status: 500,
-    })
+fn handle_foo(request: Request) -> Result<Response, HttpError> {
+    let keyvalue =
+        Keyvalue::open("my-container").map_err(|e| HttpError::UnexpectedError(e.to_string()))?;
+    match keyvalue.get("key") {
+        Err(KeyvalueError::KeyNotFound(_)) => Ok(Response {
+            headers: Some(request.headers),
+            body: Some("Key not found".as_bytes().to_vec()),
+            status: 404,
+        }),
+        Ok(value) => Ok(Response {
+            headers: Some(request.headers),
+            body: Some(value),
+            status: 200,
+        }),
+        Err(e) => Err(HttpError::UnexpectedError(e.to_string())),
+    }
 }
 
 #[register_handler]
-fn handle_bar(request: Request) -> Result<Response, Error> {
+fn handle_bar(request: Request) -> Result<Response, HttpError> {
     assert_eq!(request.method, Method::Put);
     if let Some(body) = request.body {
-        let kv = crate::Kv::open("my-container").unwrap();
-        kv.set("key", &body).unwrap();
+        let keyvalue = Keyvalue::open("my-container")
+            .map_err(|e| HttpError::UnexpectedError(e.to_string()))?;
+        keyvalue
+            .set("key", &body)
+            .map_err(|e| HttpError::UnexpectedError(e.to_string()))?;
     }
     Ok(Response {
         headers: Some(request.headers),
         body: None,
-        status: 200,
+        status: 204,
     })
 }
 
 #[register_handler]
-fn delete_file_handler(request: Request) -> Result<Response, Error> {
+fn delete_file_handler(request: Request) -> Result<Response, HttpError> {
     assert_eq!(request.method, Method::Delete);
     Ok(Response {
         headers: Some(request.headers),
@@ -70,9 +80,8 @@ fn delete_file_handler(request: Request) -> Result<Response, Error> {
     })
 }
 
-
 #[register_handler]
-fn upload(request: Request) -> Result<Response, Error> {
+fn upload(request: Request) -> Result<Response, HttpError> {
     assert_eq!(request.method, Method::Post);
     Ok(Response {
         headers: Some(request.headers),
