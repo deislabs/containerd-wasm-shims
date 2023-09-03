65ba055619044132e7c86821c220642027765d46
jiaxiao zhou <jiazho@microsoft.com>
2023-09-03 05:10:55 +0000

    feat: add -v flag to all shims
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- containerd-shim-spin-v1/src/main.rs
+++ containerd-shim-spin-v1/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::net::SocketAddr;
 use std::net::ToSocketAddrs;
 use std::option::Option;
@@ -86,7 +87,20 @@ fn parse_addr(addr: &str) -> Result<SocketAddr> {
     Ok(addrs)
 }
 
+fn parse_version() {
+    let os_args: Vec<_> = env::args_os().collect();
+    let flags = shim::parse(&os_args[1..]).unwrap();
+    if flags.version {
+        println!("{}:", os_args[0].to_string_lossy());
+        println!("  Version: {}", env!("CARGO_PKG_VERSION"));
+        println!("  Revision: {}", env!("CARGO_GIT_HASH"));
+        println!();
+        std::process::exit(0);
+    }
+}
+
 fn main() {
+    parse_version();
     shim::run::<ShimCli<Wasi>>("io.containerd.spin.v1", None);
 }
 
