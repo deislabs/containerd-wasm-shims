a3ccec0714da3001aead521b8bbd0883fa959756
jiaxiao zhou <jiazho@microsoft.com>
2023-11-07 03:05:29 +0000

    update Cargo.lock and fix some bugs in Makefile
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- containerd-shim-wws/src/main.rs
+++ containerd-shim-wws/src/main.rs
@@ -4,5 +4,5 @@ use containerd_shim_wasm::sandbox::cli::{revision, shim_main, version};
 mod engine;
 
 fn main() {
-    shim_main::<Instance<engine::WwsEngine>>("wws", version!(), revision!(), None);
+    shim_main::<Instance<engine::WwsEngine>>("wws", version!(), revision!(), None, None);
 }
