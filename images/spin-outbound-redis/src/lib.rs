47aaea6fb3e82307755d569cd10dcd220b2456a6
Jiaxiao Zhou <duibao55328@gmail.com>
2023-11-08 22:46:21 -0800

    chore(all): Bump 0.9.3 (#178)
    * chore(all): bump to 0.9.3
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>
    
    * updated lock files
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>
    
    ---------
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- images/spin-outbound-redis/src/lib.rs
+++ images/spin-outbound-redis/src/lib.rs
@@ -1,34 +1,42 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use spin_sdk::{
-    http::{internal_server_error, Request, Response},
+    http::responses::internal_server_error,
+    http::{IntoResponse, Request, Response},
     http_component, redis,
 };
+
 
 const REDIS_ADDRESS_ENV: &str = "REDIS_ADDRESS";
 const REDIS_CHANNEL_ENV: &str = "REDIS_CHANNEL";
 
 #[http_component]
-fn hello_world(_req: Request) -> Result<Response> {
+fn hello_world(_req: Request) -> Result<impl IntoResponse> {
     let address = std::env::var(REDIS_ADDRESS_ENV)?;
     let channel = std::env::var(REDIS_CHANNEL_ENV)?;
 
+    let conn = redis::Connection::open(&address)?;
+
     // Get the message to publish from the Redis key "mykey"
-    let payload = redis::get(&address, "mykey").map_err(|_| anyhow!("Error querying Redis"))?;
+    let payload = conn
+        .get("mykey")
+        .map_err(|_| anyhow!("Error querying Redis"))?
+        .context("no value for key 'mykey'")?;
 
     // Set the Redis key "spin-example" to value "Eureka!"
-    redis::set(&address, "spin-example", &b"Eureka!"[..])
+    conn.set("spin-example", &"Eureka!".to_owned().into_bytes())
         .map_err(|_| anyhow!("Error executing Redis set command"))?;
 
     // Set the Redis key "int-key" to value 0
-    redis::set(&address, "int-key", format!("{:x}", 0).as_bytes())
+    conn.set("int-key", &format!("{:x}", 0).into_bytes())
         .map_err(|_| anyhow!("Error executing Redis set command"))?;
-    let int_value = redis::incr(&address, "int-key")
+    let int_value = conn
+        .incr("int-key")
         .map_err(|_| anyhow!("Error executing Redis incr command",))?;
     assert_eq!(int_value, 1);
 
     // Publish to Redis
-    match redis::publish(&address, &channel, &payload) {
-        Ok(()) => Ok(http::Response::builder().status(200).body(None)?),
-        Err(_e) => internal_server_error(),
+    match conn.publish(&channel, &payload) {
+        Ok(()) => Ok(Response::new(200, ())),
+        Err(_e) => Ok(internal_server_error()),
     }
 }
